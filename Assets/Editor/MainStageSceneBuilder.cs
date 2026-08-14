6ea941f44729f9e24ecf803e85f26b60965e15dc
bq24014-cmd <bq24014@shibaura-it.ac.jp>
2026-08-14 20:57:20 +0900

    Landing3の着地難易度を緩和

--- Assets/Editor/MainStageSceneBuilder.cs
+++ Assets/Editor/MainStageSceneBuilder.cs
@@ -48,7 +48,7 @@ namespace HimoHitoEditor
 
             GameObject landingThree = CreatePlatform(
                 "Main Landing 3",
-                new Vector2(47.2f, -0.5f),
+                new Vector2(46.7f, -0.5f),
                 new Vector2(4f, 0.7f));
             landingThree.AddComponent<MainStageSectionTarget>();
 
