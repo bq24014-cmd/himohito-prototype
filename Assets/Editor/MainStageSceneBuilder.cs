74bd937cdfadfd00c9f321c27321e3916a606fb3
bq24014-cmd <bq24014@shibaura-it.ac.jp>
2026-08-15 16:07:45 +0900

    分岐前のLanding3を右へ延長

--- Assets/Editor/MainStageSceneBuilder.cs
+++ Assets/Editor/MainStageSceneBuilder.cs
@@ -48,8 +48,8 @@ namespace HimoHitoEditor
 
             CreatePlatform(
                 "Main Landing 3",
-                new Vector2(46.3f, -0.5f),
-                new Vector2(4f, 0.7f));
+                new Vector2(47.3f, -0.5f),
+                new Vector2(6f, 0.7f));
 
             GameObject midpoint = CreateSectionsFourAndFive();
 
