7032adddcffcd7589fcd0f41ee856b814da20996
bq24014-cmd <bq24014@shibaura-it.ac.jp>
2026-09-02 09:04:17 +0900

    第6区間の振り子用フックを左上へ移動する

--- Assets/Scripts/MainStageSectionSixSetup.cs
+++ Assets/Scripts/MainStageSectionSixSetup.cs
@@ -57,11 +57,11 @@ namespace HimoHito
             new Vector2(2.6f, 0.7f);
 
         public static readonly Vector2 LowerHookAPosition =
-            new Vector2(121.4f, 0.5f);
+            new Vector2(119.4f, 2.5f);
         public static readonly Vector2 LowerHookBPosition =
-            new Vector2(128.8f, 2.9f);
+            new Vector2(126.8f, 4.9f);
         public static readonly Vector2 LowerHookCPosition =
-            new Vector2(135.6f, 3.1f);
+            new Vector2(133.6f, 5.1f);
         public static readonly Vector2 NormalHookSize =
             new Vector2(1.6f, 0.45f);
 
